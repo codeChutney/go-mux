7ea7435e11ebdf74e8c8bdab1168961b3040b20f
codeChutney <lalwani.amith@gmail.com>
2020-10-21 22:37:07 -0500

    fix sequence query

--- main_test.go
+++ main_test.go
@@ -18,6 +18,7 @@ func TestMain(m *testing.M) {
 	fmt.Printf("Start TestMain func with %v and %T\n", m, m)
 	code := m.Run()
 	fmt.Printf("After running TestMain code is %v and it is of type %T\n", code, code)
+	clearTable()
 	os.Exit(code)
 }
 
@@ -26,6 +27,12 @@ func ensureTableExists() {
 	if _, err := a.DB.Exec(tableCreationQuery); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func clearTable() {
+
+	a.DB.Exec("DELETE FROM products")
+	a.DB.Exec("ALTER SEQUENCE products_id_seq RESTART WITH 1")
 }
 
 const tableCreationQuery = `CREATE TABLE IF NOT EXISTS products 
